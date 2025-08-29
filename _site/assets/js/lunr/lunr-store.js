a81b342071f4177ed832d2ed53c13f44340e75ce
cjim <80671370+chaejinims2@users.noreply.github.com>
2025-08-29 06:34:31 +0000

    First design done!

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "About",
+        "excerpt":"About Me I’m Chaejin Im, a system software engineer from South Korea. Currently working at Exicon, I design scalable NVMe drivers and tools for memory testers. I’m also passionate about building personal tooling, learning systems programming, and exploring creative automation. 💻 Current Stack: C/C++, Linux Kernel, Bash, Makefile, QMake 🌐...","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Projects",
+        "excerpt":"🛠 Projects Here is a curated list of my personal and professional projects. Each project reflects my hands-on experience in system software, storage drivers, and automation tools. Cherry Utility A personal CLI toolkit for task automation, backup scripts, and development workflows. Tech Stack: C++11, Bash, CLI, Linux SSINK An image-processing...","categories": [],
+        "tags": [],
+        "url": "/projects/",
+        "teaser": null
+      },]
